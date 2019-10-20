2c03dcf3c8dd3daf11e1322220101058e0a27af7
FlutterDevBrasil <devflutterbrasil@gmail.com>
2019-10-20 17:56:12 -0300

    Hello World - Projeto inicial

---
+++ hello_word/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: Text("Hello World!"),
+    ));
+  }
+}
